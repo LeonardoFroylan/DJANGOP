0244e391752c8f64f00269af85b8d18ee0f11669
LeonardoFroylan <l2225010064@guasave.tecnm.mx>
2026-03-23 08:22:24 -0700

    cambio del dia 23/03/2026

--- pythondj/blog/views.py
+++ pythondj/blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Podt
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Podt.objects.all()
+    return render(request, 'blog/post_list.html', {'Posts': posts})
 # Create your views here.
+
